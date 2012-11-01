25269e7b34c65bd27e6c2e867ae89dadba169f86
Jonathan Maltz <jm1271@gmail.com>
2012-11-01 12:21:34 -0400

    startup script working

---
+++ nodeServer/bin/startup.js
@@ -0,0 +1,26 @@
+var cronJob = require('cron').CronJob;
+var config = require('../config').config;
+var redditObj = require('../redditRequest');
+
+var subreddits = config.testSubreddits;
+
+
+for(var i = 0; i< subreddits.length; i++){ //for all the of the subreddits that we want to test
+		var cronOptions = {
+				cronTime: '* * * * * *',
+				onComplete: null,
+				onTick: redditUpdateJob(subreddits[i]),
+				start: true,
+				timezone: "America/Los Angeles"
+		};
+		console.log(subreddits[i]);
+		new cronJob('0/30 * * * * *', 
+								redditUpdateWrapper(subreddits[i]), null, true, "America/Los_Angeles");
+}
+
+function redditUpdateJob(subreddit){
+		redditObj.update(subreddit);
+}
+
+
+function redditUpdateWrapper(j) { return function() { redditUpdateJob(j); } }
---
+++ nodeServer/config.js
@@ -0,0 +1,8 @@
+var config = {};
+
+config.subreddits = ['pics', 'funny', 'videos', 'bestof', 'fffffffuuuuuuuuuuuu', 'askreddit', 'gaming', 'starcraft', 'todayilearned', 'adviceanimals'];
+config.testSubreddits = ['fffffffuuuuuuuuuuuu', 'adviceanimals'];
+
+
+
+exports.config = config;
