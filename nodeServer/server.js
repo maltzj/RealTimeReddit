75acc06c36aa7833141d91f88f69c30c76d1321b
Jonathan Maltz <jm1271@gmail.com>
2012-11-03 21:41:03 -0400

    Added a testing webpage so that I can get the sockets working

--- nodeServer/server.js
+++ nodeServer/server.js
@@ -1,10 +1,20 @@
 var http = require("http");
 var ioModule = require("./sockets");
+var fs = require("fs");
+
 function startServer(callback){
 		var server = http.createServer(function(request, response){
-				response.writeHead('200');
-				response.write('Success');
-				response.end();
+				fs.readFile(__dirname + '/index.html', function(error, data){
+						if(error){
+								response.writeHead('500');
+								response.write('Fail Whale :(');
+						}
+						else{
+								response.writeHead('200');
+								response.write(data);
+						}
+						response.end();
+				});	
 		});
 		ioModule.listen(server);
 		server.listen(8080);
